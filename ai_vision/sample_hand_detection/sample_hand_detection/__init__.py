cd08fcd94ca35b4c8e17029121bd16960b3d1c37
DapengYuan-David <yuand1994@gmail.com>
2025-05-19 06:11:33 +0000

    sample_hand_detection: update license
    Signed-off-by: Dapeng_Yuan <quic_dapeyuan@quicinc.com>
    Signed-off-by: DapengYuan-David <yuand1994@gmail.com>

--- ai_vision/sample_hand_detection/sample_hand_detection/__init__.py
+++ ai_vision/sample_hand_detection/sample_hand_detection/__init__.py
@@ -1,2 +1,2 @@
-# Copyright (c) 2025 Qualcomm Innovation Center, Inc. All rights reserved.
+# Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
 # SPDX-License-Identifier: BSD-3-Clause-Clear
